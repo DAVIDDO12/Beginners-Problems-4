86575ceba76654cb382f71c71d6b1c59ee1172d4
DAVIDDO12 <168615558+DAVIDDO12@users.noreply.github.com>
2024-05-08 11:56:57 +1200

    Update Iteration.py

--- Iteration.py
+++ Iteration.py
@@ -1,7 +1,7 @@
 a = int(input("Enter the number: ")) 
 b = int(input("Enter the range: "))
-for i in range(1, b + 1):
-    print(f"{a} x {i} = {a * i}")
+for num in range(1, b + 1):
+    print(f"{a} x {num} = {a * num}")
 if a<0:
     print("Not a positive integer.Enter a positive integer.")
 if b<0:

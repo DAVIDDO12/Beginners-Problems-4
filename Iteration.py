6b59a5d14098c8b5295dbd62eb88ffd2db24f401
DAVIDDO12 <168615558+DAVIDDO12@users.noreply.github.com>
2024-05-13 14:37:17 +1200

    Update Iteration.py

--- Iteration.py
+++ Iteration.py
@@ -2,11 +2,12 @@ a = int(input("Enter the number: "))
 b = int(input("Enter the range: "))
 if a<0:
     print("Not a positive integer.Enter a positive integer.")
+else:
+    for num in range(1, b + 1):
+     multiplier=a * num
+     print(multiplier)
 if b<0:
     print("Not a positive integer.Enter a positive integer.")
-for num in range(1, b + 1):
-    multiplier=a * num
-    print(multiplier)
 
 count = 0
 number=int(input("Enter a number:"))
